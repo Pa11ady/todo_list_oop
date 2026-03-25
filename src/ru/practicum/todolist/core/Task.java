7dc30e8a3a200f3bb70fd887bad79815f87d27d4
Pavel <pa11ady@yandex.ru>
2026-03-26 01:00:00 +0300

    Task

--- src/ru/practicum/todolist/core/Task.java
+++ src/ru/practicum/todolist/core/Task.java
@@ -1,12 +1,22 @@
 package ru.practicum.todolist.core;
 
+import java.time.LocalDateTime;
+
 public class Task {
+    private int id = 0;
     private String name;
     private int priority;
+    private Status status = Status.UNCOMPLETED;
+    private final LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime completedAt;
 
     public Task(String name, int priority) {
         this.name = name;
         this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public int getPriority() {
@@ -17,6 +27,10 @@ public class Task {
         return name;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -25,11 +39,25 @@ public class Task {
         this.priority = priority;
     }
 
+    public void setStatus(Status status) {
+        this.status = status;
+        if (status == Status.COMPLETED) {
+            this.completedAt = LocalDateTime.now();
+        } else  {
+            this.completedAt = null;
+        }
+        // Для IN_PROGRESS completedAt логично тоже сбрасывать, возможно опечатка в тз
+    }
+
     @Override
     public String toString() {
         return "Task{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", priority=" + priority +
+                ", status=" + status +
+                ", created=" + created +
+                ", completedAt=" + completedAt +
                 '}';
     }
 }
